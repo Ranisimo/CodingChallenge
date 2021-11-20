b60ab964b3825ae9576485e1f4fd835a9a3c0e00
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-11-20 19:39:59 +0000

    created basic machine and employee instances

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-import Subject from 'libs/subject.js';
-import Observer from 'libs/observer.js';
+import {Machine} from './libs/subject.js';
+import {Employee} from './libs/observer.js';
 
 function init() {
-    const employee = new Employee("john", "technician");
-    const machine1 = new Machine("1", "idle")
+    const employee1 = new Employee("john", "technician");
+    const machine1 = new Machine("idle", "components")
 
-    machine1.attachObserver(employee);
-    machine1.notifyAllObservers()
-}
+    machine1.attachObserver(employee1);
+    machine1.notifyAllObservers(machine1._name + ' ' + machine1._state);
+    console.log(machine1.observers);
+};
 
-init()
+init();
 /* function periodicStateChange() {
 
 }; */
